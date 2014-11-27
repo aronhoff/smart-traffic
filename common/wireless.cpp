162a8d9d1f0557f80f2ba4a9c6c1b5f1371835ef
Aron Hoffmann <haam97@gmail.com>
2014-11-27 19:21:32 +0000

    Backup commit, changing approach

--- common/wireless.cpp
+++ common/wireless.cpp
@@ -5,11 +5,12 @@ Wireless::Wireless(HardwareSerial& serial) : mSerial(serial) {
 }
 
 void Wireless::init() {
-	mToSendInit = true;
-}
-
-void Wireless::initSystemTime() {
-	mToSendInitSystemTime = true;
+	writeHeader();
+	write(messageInit);
+	write(0);
+	writeTime(0);
+	writeFooter();
+	
 }
 
 void Wireless::sendMotorUpdate(uint64_t time, int8_t speed, int8_t steer) {
@@ -29,39 +30,134 @@ void Wireless::sendBatteryWarning() {
 	mToSendBatteryWarning = true;
 }
 
+void Wireless::sendMessageIncorrect() {
+	mToSendMessageIncorrect = true;
+	while(available()) {
+		if(available() < 2) mSerial.read();
+		else {
+			if(mSerial.read() != 0x00) continue;
+			else if(mSerial.read() != 0xFE) continue;
+		}
+		
+
 void Wireless::send() {
-	mSerial.write(0);
+	// Header
+	writeHeader();
+	// Message
 	if(mToSendInit) 
-		mSerial.write(messageInit);
-	if(mToSendInitSystemTime)
-		mSerial.write(messageSystemTime);
+		write(messageInit);
 	if(mToSendMotorUpdate) {
-		mSerial.write(messageMotorUpdate);
-		for(int i = 4; i >= 0; i++) 
-			mSerial.write((uint8_t)(mMotorUpdateTime >> (i*8)) & 0xFF);
-		mSerial.write(mMotorUpdateMotorSpeed);
-		mSerial.write(mMotorUpdateSteerPosition);
+		write(messageMotorUpdate);
+		writeTime(mMotorUpdateTime);
+		write(mMotorUpdateMotorSpeed);
+		write(mMotorUpdateSteerPosition);
 	}
 	if(mToSendRequestMotorState) {
-		mSerial.write(messageMotorRequest);
-		mSerial.write(mRequestMotorStateSpeed);
-		mSerial.write(mRequestMotorStateSteerPosition);
+		write(messageMotorRequest);
+		write(mRequestMotorStateSpeed);
+		write(mRequestMotorStateSteerPosition);
 	}
 	if(mToSendBatteryWarning)
-		mSerial.write(messageBattery);
-	mSerial.write(0xFF);
-
+		write(messageBattery);
+	if(mToSendMessageIncorrect)
+		write(messageMessageIncorrect);
+	// End
+	writeFooter();
+	// Reset
 	clean();
 }
 
 void Wireless::receive() {
-	
+	// Nothing to read?
+	if(!available()) 
+		return;
+	//Header
+	if(!readHeader())
+		goto incorrectMessage; // !!! NOT BEGINNING OF MESSAGE
+	// Message
+	while(mSerial.peek() != 0xFE) {
+		switch(mSerial.read()) {
+			case messageInit:
+				break;
+			case messageMotorUpdate:
+				uint64_t time = readTime();
+				int8_t speed, steer;
+				speed = mSerial.read();
+				steer = mSerial.read();
+				break;
+			case messageMotorRequest:
+				int8_t speed, steer;
+				speed = mSerial.read();
+				steer = mSerial.read();
+				break;
+			case messageBattery:
+				break;
+			case messageMessageIncorrect:
+				break;
+			case -1:
+				break;
+			default:
+				goto incorrectMessage;
+				break;
+		}
+	}
+	// End
+	if(!readFooter())
+		goto incorrectMessage; // !!! MESSAGE NOT COMPLETE
+
+	// More messages?
+	receive();
+	return;
+
+incorrectMessage:
+	sendMessageIncorrect();
+	return;
 }
 
 void Wireless::clean() {
 	mToSendInit = false;
-	mToSendInitSystemTime = false;
 	mToSendMotorUpdate = false;
 	mToSendRequestMotorState = false;
 	mToSendBatteryWarning = false;
+	mToSendMessageIncorrect = false;
+}
+void Wireless::write(uint8_t x) {
+	mSerial.write(x);
+}
+uint8_t Wireless::read() {
+	return mSerial.read();
+}
+int Wireless::available() {
+	return mSerial.available();
+}
+void Wireless::writeHeader() {
+	mSerial.write(0x00);
+	mSerial.write(0xFE);
+}
+void Wireless:writeFooter() {
+	mSerial.write(0xFE);
+	mSerial.write(0xFF);
+}
+bool Wireless::readHeader() {
+	return mSerial.read() == 0x00
+		&& mSerial.read() == 0xFE;
+}
+bool Wireless::readFooter() {
+	return mSerial.read() == 0xFE
+		&& mSerial.read() == 0xFF;
+}
+void Wireless::writeTime(uint64_t time) {
+	for(int i = 4; i >= 0; i--) 
+		write((uint8_t)(mMotorUpdateTime >> (i*8)) & 0xFF);
+}
+uint64_t Wireless::readTime() {
+	uint64_t ret = 0;
+	for(int i = 0; i < 5; i++) {
+		ret |= read();
+		ret << 8;
+	}
+}
+
+void Wireless::setSetTime(voidLongCall call) {
+	setTime = call;
 }
--- common/wireless.h
+++ common/wireless.h
@@ -20,13 +20,24 @@ class Wireless {
 		HardwareSerial& mSerial;
 
 		void clean();
+		void writeHeader();
+		void writeFooter();
+		bool readHeader();
+		bool readFooter();
+		void writeTime(uint64_t);
+		uint64_t readTime();
+		void write(uint8_t x);
+		uint8_t read();
 
 		bool mToSendInit = false;
 		bool mToSendInitSystemTime = false;
 		bool mToSendMotorUpdate = false;
 		bool mToSendRequestMotorState = false;
 		bool mToSendBatteryWarning = false;
-		
+		bool mToSendMessageIncorrect = false;
+
+		voidLongCall setTime;
+
 		// Only using 5 bytes for the time!
 		uint64_t mMotorUpdateTime;
 		int8_t mMotorUpdateMotorSpeed;
@@ -36,28 +47,50 @@ class Wireless {
 		int8_t mRequestMotorStateSteerPosition;
 
 		static const int8_t messageInit = 1;
-		static const int8_t messageSystemTime = 2;
-		static const int8_t messageMotorUpdate = 3;
-		static const int8_t messageMotorRequest = 4;
-		static const int8_t messageBattery = 5;
+		static const int8_t messageMotorUpdate = 2;
+		static const int8_t messageMotorRequest = 3;
+		static const int8_t messageBattery = 4;
+		static const int8_t messageMessageIncorrect = 5;
 
 	public:
 		Wireless(HardwareSerial& serial);
+
+		int available();
 
 		// Server -> car	Place car on starting position
 		void init();
 		// Server -> car	Initialize system time
 		void initSystemTime();
+		// Server -> car	Inform car about a new obstacle
+//		void sendObstacle(/*position*/);
+		// Server -> car	Inform car about obstacle removed
+//		void removeObstacle(/*position*/);
+		// Server -> car	Inform car about position sensor result
+//		void updatePosition(/*time, position*/);
 		// Car -> server	Send new motor data
 		void sendMotorUpdate(uint64_t time, int8_t motor, int8_t steer);
 		// Server -> car	Request motor operation
 		void requestMotorState(int8_t motor, int8_t steer);
+		// 
+//!!!!!		void setRoute(/*routeID*/);
+		// Server -> car	Signals a new pedestrian at position
+//		void putPedestrian();
+		// Server -> car	Pedestrian removed
+//		void removePedestrian();
+		// Server -> car	Give permission for the next crossing
+//		void allowCrossing();
 		// Car -> server	Send low battery warning
 		void sendBatteryWarning();
+		// Both				Message not received correctly
+		void sendMessageIncorrect();
 		// Both				Sends outstanding data
 		void send();
 
 		// Both				Receive outstanding data
 		void receive();
+
+		typedef void (*voidCall)();
+		typedef void (*voidLongCall)(uint64_t);
+		void setSetTime(voidLongCall call);
 };
 #endif
